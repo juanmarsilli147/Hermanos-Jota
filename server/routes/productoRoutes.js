3ebe7e7dc8cd19763f29668d2c558eb935e78a85
Juan Marsilli <juanmarsilli147@gmail.com>
2025-10-03 12:20:06 -0300

    Ruta para id de productos

--- server/routes/productoRoutes.js
+++ server/routes/productoRoutes.js
@@ -7,4 +7,17 @@ routes.get('/', (req, res) => {
     res.json(productos)
 })
 
+routes.get("/:id", (req, res, next) => {
+    const { id } = req.params
+    const producto = productos.find(p => p.id === parseInt(id))
+
+    if (!producto) {
+        const error = new Error(`Producto no encontrado`);
+        error.status = 404;
+        return next(error); 
+  }
+
+    res.json(producto)
+})
+
 module.exports = routes
