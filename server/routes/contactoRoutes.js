5511e325ff5e2072f4babada18e9e7a4179fdcea
facu_v <facundoandreassi@gmail.com>
2025-10-03 12:50:08 -0300

    Gestión errores POST Contacto
    Se agregó la funcionalidad de gestión de errores cuando la api de contacto recibe un mensaje inválido. Se aplicó la misma verificación que en el frontend.

--- server/routes/contactoRoutes.js
+++ server/routes/contactoRoutes.js
@@ -9,18 +9,42 @@ routes.get('/', (req, res) => {
 })
 
 // POST /api/contacto
-routes.post('/', (req, res) => {
-    const nuevoMensaje = req.body
+routes.post('/', (req, res, next) => {
 
-    console.log('Mensaje recibido:', nuevoMensaje)
+    try {
+        const {nombre, email, mensaje} = req.body
 
-    mensajes.push(nuevoMensaje)
-    res.status(201).json(
-        {
-            mensaje: "Mensaje recibido",
-            texto: nuevoMensaje
+        //Se valida que se hayan ingresado todos los campos
+        if (!nombre || !email || !mensaje) {
+            return res.status(400).json({ error: "Todos los campos son obligatorios."})
         }
-    )
+
+        //Se valida que el nombre sea válido usando la misma expresión regular que el front
+        const regexNombre = /^[\p{L}\s]+$/u
+        if (!regexNombre.test(nombre)) {
+            return res.status(400).json({ error: "Formato de nombre inválido."})
+        }
+        
+        //Se valida que el mail sea válido con la misma expresión regular que el front
+        const regexMail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        if (!regexMail.test(email)) {
+            return res.status(400).json({ error: "Formato de email inválido."})
+        }
+
+        const nuevoMensaje = {nombre, email, mensaje}
+        mensajes.push(nuevoMensaje)
+        console.log('Mensaje recibido:', nuevoMensaje)
+        
+        res.status(201).json(
+            {
+                mensaje: "Mensaje recibido",
+                texto: nuevoMensaje
+            }
+        )
+    } catch(error) {
+        next(error)
+    }
+    
 })
 
 module.exports = routes
