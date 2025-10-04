af21b5cb2d66e796fd5a6421f7a939fd2fdf7b94
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-10-04 02:44:35 -0300

    footerComponent

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import './css/App.css';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      
+      <Footer/>
     </>
   );
 }
