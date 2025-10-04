a6a92a53329e81149bc0d9fb682f005466122864
Juan Marsilli <juanmarsilli147@gmail.com>
2025-10-04 11:17:00 -0300

    Eliminacion de componentes externos a la seccion de catalogo
    Elimino componentes de contacto y de index para mantener la rama ligada unicamente a la parte de productos y no interferir con las demas ramas

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,10 @@
 import './css/App.css';
-import ContactoTexto from './components/ContactoTexto';
-import ContactoFormulario from './components/ContactoFormulario';
-import Navbar from './components/NavBar';
-import Footer from './components/Footer';
 import Catalogo from './pages/Catalogo';
 
 function App() {
   return (
     <>
-      <Navbar />
       <Catalogo />
-      <Footer />
     </>
   );
 }
