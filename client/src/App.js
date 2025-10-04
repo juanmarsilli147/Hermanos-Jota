25c56e76c2e054464e5efab2f86fd8b8355f3d39
facu_v <facundoandreassi@gmail.com>
2025-10-04 13:06:19 -0300

    Header & Footer
    Se trajeron los componentes del header y footer de la rama index para verificar compatibilidad. Actualización del css.

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import './css/App.css';
 import Contacto from './pages/Contacto';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-
+      
+      <Header />
       <Contacto />
+      <Footer />
 
     </div>
   );
