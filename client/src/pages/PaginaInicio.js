c8af64c115d3b0c3a2019801883cd139935b9845
Marcos Cazzulino <marcoscazzulino@gmail.com>
2025-10-05 22:44:24 -0300

    fix-ver-detalle-index

--- client/src/pages/PaginaInicio.js
+++ client/src/pages/PaginaInicio.js
@@ -23,7 +23,7 @@ function PaginaInicio({setRoute, anadirFuncion}){
                 </>
             ) : (
                 <section className='catalogo'>
-                    <ProductDetail key={productoSeleccionado.id} producto={productoSeleccionado} onVolver={() => setProductoSeleccionado(null)} anadirFuncion={anadirFuncion}/>
+                    <ProductDetail key={productoSeleccionado.id} producto={productoSeleccionado} onVolver={() => setProductoSeleccionado(null)} anadirFuncion={anadirFuncion} onVerDetalle={setProductoSeleccionado}/>
                 </section>
             )}
         </main>
